61959fef344ddc3d66cee90604280fb2be8bec35
yonghee12 <suncar1000@gmail.com>
2020-08-06 02:19:49 +0900

    s4_1920_수 찾기.py

--- 02_baekjoon/s4_1920_수 찾기.py
+++ 02_baekjoon/s4_1920_수 찾기.py
@@ -2,8 +2,10 @@
 
 import sys
 
+
 def read():
     return sys.stdin.readline().strip()
+
 
 n = int(read())
 a = read().split(' ')
@@ -12,3 +14,17 @@ b = read().split(' ')
 
 dic = {k: 1 for k in a}
 sys.stdout.write('\n'.join([str(int(k in dic)) for k in b]))
+
+
+def main():
+    input()
+    n = input().strip().split(' ')
+    input()
+
+    s = set(n)
+    r = ''
+    for k in input().strip().split(' '):
+        r += '1\n' if k in s else '0\n'
+    print(r)
+
+main()
